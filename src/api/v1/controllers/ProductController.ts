8ea3a5eacb91e660d1034dedd64bf543ad593621
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 13:09:24 -0500

    Completed getAll function for kakanin.

--- src/api/v1/controllers/ProductController.ts
+++ src/api/v1/controllers/ProductController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from 'express';
 import { HTTP_STATUS } from "../../../constants/httpConstants"
 import { successResponse } from "../models/responseModel"
+import { sampleKakanin } from "../models/sampleData";
 
 // import {} from "../services/ProductService";
 
 export const getAllKakanin = async (req: Request, res: Response) => {
-
+    try {
+        const kakanin = sampleKakanin; // Replace with actual service call in the future
+        
+        res.status(HTTP_STATUS.OK).json(successResponse(kakanin));
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Something went wrong getting the kakanin inventory."});
+    }
 };
