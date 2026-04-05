78dcdb14e4355cce6273b823bd105a0e196865f1
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 18:54:08 -0500

    connected getOrderById to routes.

--- src/api/v1/routes/orderRoutes.ts
+++ src/api/v1/routes/orderRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAllOrders } from '../controllers/orderController';
+import { getAllOrders, getOrderById } from '../controllers/orderController';
 
 const orderRoutes = Router();
 
 orderRoutes.get("/", getAllOrders);
+orderRoutes.get("/:id", getOrderById);
 
 export default orderRoutes;
