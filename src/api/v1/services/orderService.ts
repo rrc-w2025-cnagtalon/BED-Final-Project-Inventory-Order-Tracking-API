282cf6c4c8406e3dca0c0c5e05b108e90c533234
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 18:51:57 -0500

    Added getOrderByIdService to orderService file.

--- src/api/v1/services/orderService.ts
+++ src/api/v1/services/orderService.ts
@@ -5,3 +5,6 @@ export const getAllOrdersService = async (): Promise<OrderSlip[] | undefined> =>
     return sampleOrderSlips; // Replace with actual database call in the future
 };
 
+export const getOrderByIdService = async (id: string): Promise<OrderSlip | undefined> => {
+    return sampleOrderSlips.find((order) => order.orderNumber === id); // Replace with actual database call in the future
+}
