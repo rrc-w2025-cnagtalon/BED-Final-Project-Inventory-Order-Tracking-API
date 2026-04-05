b25dea96ba4e92d801b051736d41f0058f8f0e1a
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 14:45:14 -0500

    Connected updateKakanin to routes.

--- src/api/v1/routes/productRoutes.ts
+++ src/api/v1/routes/productRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAllKakanin, getKakaninById, createKakanin} from "../controllers/productController";
+import { getAllKakanin, getKakaninById, createKakanin, updateKakanin} from "../controllers/productController";
 
 const productRoutes = Router();
 
 productRoutes.get("/", getAllKakanin); 
 productRoutes.get("/:id", getKakaninById);
 productRoutes.post("/", createKakanin);
+productRoutes.put("/:id", updateKakanin);
 
 export default productRoutes;
