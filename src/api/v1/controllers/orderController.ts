5c43899a622497f15c6ab789b0f5cbde17cc812c
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 18:43:39 -0500

    Created orderController file.

---
+++ src/api/v1/controllers/orderController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+import { errorResponse, successResponse } from "../models/responseModel";
+
+
+// get all orders
+export const getAllOrders = async (req: Request, res: Response): Promise<void> => {
+    try {
+        //const orders = await getAllOrdersService();
+        
+        res.status(HTTP_STATUS.OK).json(successResponse([], "Here is the current list of orders."));
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json(errorResponse("Something went wrong getting the orders.", "GET_ORDERS_ERROR"));
+    }
+};
