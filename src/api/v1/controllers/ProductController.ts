1ab55b244b1f7cc4ecd63ab519424c7c989b8df4
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 13:29:03 -0500

    Modified controller to use errorHandler.

--- src/api/v1/controllers/ProductController.ts
+++ src/api/v1/controllers/ProductController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { HTTP_STATUS } from "../../../constants/httpConstants"
-import { successResponse } from "../models/responseModel"
+import { errorResponse, successResponse } from "../models/responseModel"
 import { sampleKakanin } from "../models/sampleData";
 
 // import {} from "../services/ProductService";
@@ -11,6 +11,6 @@ export const getAllKakanin = async (req: Request, res: Response) => {
         
         res.status(HTTP_STATUS.OK).json(successResponse(kakanin));
     } catch (error) {
-        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Something went wrong getting the kakanin inventory."});
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json(errorResponse("Something went wrong getting the kakanin inventory.", "GET_INVENTORY_ERROR"));
     }
 };
