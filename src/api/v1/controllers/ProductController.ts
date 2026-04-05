a809ca8d6cf41d30241e692f594d530cc8f78f4a
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 12:59:39 -0500

    Imported httpConstants and successResponse to productController file.

--- src/api/v1/controllers/ProductController.ts
+++ src/api/v1/controllers/ProductController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import { HTTP_STATUS } from "../../../constants/httpConstants"
+import { successResponse } from "../models/responseModel"
 
 // import {} from "../services/ProductService";
 
 export const getAllKakanin = async (req: Request, res: Response) => {
-    
+
 };
