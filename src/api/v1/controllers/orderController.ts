43695de530fba2b2284163a4ac665bcd7fa9f641
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 18:48:31 -0500

    Connected order routes and service to controller.

--- src/api/v1/controllers/orderController.ts
+++ src/api/v1/controllers/orderController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 import { errorResponse, successResponse } from "../models/responseModel";
-
+import { getAllOrdersService } from '../services/orderService';
 
 // get all orders
 export const getAllOrders = async (req: Request, res: Response): Promise<void> => {
     try {
-        //const orders = await getAllOrdersService();
+        const orders = await getAllOrdersService();
         
         res.status(HTTP_STATUS.OK).json(successResponse([], "Here is the current list of orders."));
     } catch (error) {
--- src/api/v1/routes/orderRoutes.ts
+++ src/api/v1/routes/orderRoutes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { getAllOrders } from '../controllers/orderController';
 
 const orderRoutes = Router();
 
-// orderRoutes.get("/", getAllOrders);
+orderRoutes.get("/", getAllOrders);
 
 export default orderRoutes;
