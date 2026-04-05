fdf41ed1ab89429a121154ce2ba45405fb9c2f7a
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 12:45:12 -0500

    Added get all kakanin route.

---
+++ src/api/v1/routes/KakaninRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+// import {} from "../controllers/ProductController";
+
+const kakaninRoutes = Router();
+
+//kakaninRoutes.get("/", getAllKakanin); 
+
+export default kakaninRoutes;
