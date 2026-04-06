d0d1041cb0ed3f9cadab828af945fb055b89b5cb
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-05 19:24:15 -0500

    Connected createOrder function to route.

--- src/api/v1/routes/orderRoutes.ts
+++ src/api/v1/routes/orderRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getAllOrders, getOrderById } from '../controllers/orderController';
+import { getAllOrders, getOrderById, createOrder } from '../controllers/orderController';
 
 const orderRoutes = Router();
 
 orderRoutes.get("/", getAllOrders);
 orderRoutes.get("/:id", getOrderById);
-
+orderRoutes.post("/", createOrder);
 export default orderRoutes;
