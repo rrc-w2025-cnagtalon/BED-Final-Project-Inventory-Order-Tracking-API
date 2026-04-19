691c2f87827f9d1051a6ca2d2b46585a768760a4
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-19 03:12:34 -0500

    Added authentication and authorization to product routes.

--- src/api/v1/routes/productRoutes.ts
+++ src/api/v1/routes/productRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { getAllKakanin, getKakaninById, createKakanin, updateKakanin, deleteKakanin} from "../controllers/productController";
+import authenticate from '../middleware/authenticate';
+import isAuthorized from '../middleware/authorize';
 
 const productRoutes = Router();
 
 productRoutes.get("/", getAllKakanin); 
 productRoutes.get("/:id", getKakaninById);
-productRoutes.post("/", createKakanin);
-productRoutes.put("/:id", updateKakanin);
-productRoutes.delete("/:id", deleteKakanin);
+
+productRoutes.post("/", authenticate, isAuthorized({hasRole: ["manager", "employee"]}), createKakanin);
+productRoutes.put("/:id", authenticate, isAuthorized({hasRole: ["manager", "employee"]}), updateKakanin);
+productRoutes.delete("/:id", authenticate, isAuthorized({hasRole: ["manager"]}), deleteKakanin);
 
 export default productRoutes;
