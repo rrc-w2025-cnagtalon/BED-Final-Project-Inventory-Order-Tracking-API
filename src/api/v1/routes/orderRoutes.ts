a384354852eff1c1b7b35e08fe3243b29b890e48
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-19 01:29:46 -0500

    Added authenticate to protected routes.

--- src/api/v1/routes/orderRoutes.ts
+++ src/api/v1/routes/orderRoutes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import { getAllOrders, getOrderById, createOrder, updateOrder, deleteOrder } from '../controllers/orderController';
 import { validateRequest } from '../middleware/validate';
 import { orderSchemas } from '../validation/orderSchemas';
-
+import  authenticate  from '../middleware/authenticate';
 const orderRoutes = Router();
 
 orderRoutes.get("/", getAllOrders);
 orderRoutes.get("/:orderNumber", getOrderById);
-orderRoutes.post("/", validateRequest(orderSchemas.create), createOrder);
-orderRoutes.put("/:orderNumber", validateRequest(orderSchemas.update), updateOrder);
-orderRoutes.delete("/:orderNumber", validateRequest(orderSchemas.delete), deleteOrder);
+
+orderRoutes.post("/", authenticate, validateRequest(orderSchemas.create), createOrder);
+orderRoutes.put("/:orderNumber", authenticate, validateRequest(orderSchemas.update), updateOrder);
+orderRoutes.delete("/:orderNumber", authenticate, validateRequest(orderSchemas.delete), deleteOrder);
 
 export default orderRoutes;
