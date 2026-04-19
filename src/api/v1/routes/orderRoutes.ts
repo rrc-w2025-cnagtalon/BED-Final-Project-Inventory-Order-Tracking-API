11b1a1be9fb9d09473702bc18f08498c1f9be50a
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-04-19 02:31:44 -0500

    Added authorize middleware and added to post order route.

--- src/api/v1/routes/orderRoutes.ts
+++ src/api/v1/routes/orderRoutes.ts
@@ -3,12 +3,13 @@ import { getAllOrders, getOrderById, createOrder, updateOrder, deleteOrder } fro
 import { validateRequest } from '../middleware/validate';
 import { orderSchemas } from '../validation/orderSchemas';
 import  authenticate  from '../middleware/authenticate';
+import  isAuthorized  from '../middleware/authorize';
 const orderRoutes = Router();
 
 orderRoutes.get("/", getAllOrders);
 orderRoutes.get("/:orderNumber", getOrderById);
 
-orderRoutes.post("/", authenticate, validateRequest(orderSchemas.create), createOrder);
+orderRoutes.post("/", authenticate,  isAuthorized({hasRole: ["manager"], allowSameUser: true}), validateRequest(orderSchemas.create), createOrder);
 orderRoutes.put("/:orderNumber", authenticate, validateRequest(orderSchemas.update), updateOrder);
 orderRoutes.delete("/:orderNumber", authenticate, validateRequest(orderSchemas.delete), deleteOrder);
 
